203970b589718e3970a9f4abd02b448e4d52e6e3
Abdalfttah Mohamed <abdalftahmohamed054@gmail.com>
2024-01-02 10:23:50 +0200

    "add database to user table"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('business_name');
+            $table->string('country')->nullable();
+            $table->string('website')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('business_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('industry')->nullable();
+            $table->string('role')->nullable();
+            $table->longText('code')->nullable();
+            $table->dateTime('expire_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
